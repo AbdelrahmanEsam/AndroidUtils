3dee70c72e60ab7b90310476fab4193068894e9e
AbdelrahmanEsam <abdofahd20000@gmail.com>
2022-12-24 05:23:43 +0200

    Create FacebookSignInWithJetpackCompose.kt

---
+++ FacebookSignInWithJetpackCompose.kt
@@ -0,0 +1,15 @@
+ val rememberLauncher = rememberLauncherForActivityResult(
+        contract = LoginManager.getInstance().createLogInActivityResultContract()
+    ){
+        LoginManager.getInstance().onActivityResult(it.resultCode, it.data, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult) {
+                Toast.makeText(activity, "success", Toast.LENGTH_SHORT).show()
+            }
+            override fun onCancel() {
+                Toast.makeText(activity, "cancer", Toast.LENGTH_SHORT).show()
+            }
+            override fun onError(error: FacebookException) {
+                Toast.makeText(activity, error.message, Toast.LENGTH_LONG).show()
+            }
+        })
+    }
